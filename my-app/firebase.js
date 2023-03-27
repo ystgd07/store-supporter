cd9b33a8ba625d4e8a6d7d2966ece96687c75e98
yang <ysotgood@gmail.com>
2023-03-27 16:29:53 +0900

    enroll-등록하기 기능 구현(firebase 연동)

---
+++ my-app/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage, getStroge } from 'firebase/storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STR_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MSG_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    // measurementId: process.env.NEXT_PUBLIC_FIREBASE_MST_ID,
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage };
